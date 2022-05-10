584821065804793cbe829d38f23a0d3d7733968a
Henri Collin <henri.collin@student.uclouvain.be>
2022-05-10 14:28:41 +0200

    modifs

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {Component} from 'react';
 import * as tf from '@tensorflow/tfjs';
-tf.ENV.set('WEBGL_PACK', false)
+// tf.ENV.set('WEBGL_PACK', false)
 // import { CameraFeed } from './camera-feed';
 var axios = require('axios');
 var qs = require('qs');
@@ -33,25 +33,28 @@ class Count extends Component{
       image:null,
       model:null
     }
-    
+    this.useModel = this.useModel.bind(this)
   }
 
   async componentDidMount(){
     updateState = updateState.bind(this)
-    var json = 'http://127.0.0.1:8080/model.json'
+    var json = 'http://127.0.0.1:8080/model.json'     //"http-server -a 127.0.0.1 --cors -c60" from the backend/model
     console.log(json)
     const model = await tf.loadGraphModel(json)
     console.log(model)
     this.setState( {model: model} )
     // localStorage.setItem('json', JSON.parse(JSON.stringify(json)))
     // await require('./model/model.json').save('localstorage://json');
+
   }
 
   async onChange(e) {
     // console.log(e.target)
     var u = e.target.files[0]
+    var url = URL.createObjectURL(u)
     console.log("U : ", u)
-    this.setState({ image: URL.createObjectURL(u), display:true })
+    console.log("url :", url)
+    this.setState({ image: url, display:true })
     this.forceUpdate()
     var formData = new FormData();
     formData.append("image", u);
@@ -122,36 +125,55 @@ class Count extends Component{
   }
 
   async useModel(e){
-    // tf.setBackend('webgl', false)
+    tf.setBackend('cpu')
     var u = e.target.files[0]
+    var url = URL.createObjectURL(u)
+
     console.log("MODEL : ", this.state.model)
     console.log(u)
     var model = this.state.model
-    const image = document.createElement("img");
-    image.height = 768
-    image.width = 1024
-    image.src = u
 
-    const batchsize = 10
-    const verbose = true
-    document.body.append(image);
-    console.log("IMAGE : ",image)
-    var tensor = tf.browser.fromPixels(image)
-    console.log(tensor)
-    // var tensor2 = tensor.reshape([1,3,768,1024]).toFloat()
-    var tensor2 = tensor.expandDims(0).reshape([1,3,768,1024]).toFloat()
-    console.log(tensor2)
-    // console.log("BEFORE PREDICT")
-
-    // console.log(tf.memory())
-    // const warmup = tf.zeros([1,3,768,1024])
-    // const result = model.predict(warmup, batchsize, verbose)
-    // result.dataSync();
-    // result.dispose();
-
-
-    model.predict(tensor2, batchsize, verbose);
+    const image = new Image()
+    image.src = url
+    // var canvas = document.createElement('canvas');
+    // var context = canvas.getContext('2d');
+    // context.strokeStyle="#FFFFFF"
+    // context.drawImage(image, 700, 700);
     
+    // console.log("+++++++++++++++", context.getImageData(700,700,124,124).data)
+
+    this.setState({image: url, display:true})
+    this.forceUpdate()
+    setTimeout( () => {
+      var tensor = tf.browser.fromPixels(image);
+      var tensor3 = tensor.transpose().expandDims(0).toFloat();
+      tensor3.print()
+      var val = model.predict(tensor3);
+      console.log(val);
+      (val.sum()).print()
+    } , 2000)
+    // var tensor = tf.browser.fromPixels(image)
+    // var tensor2 = tensor.transpose([2,0,1]).expandDims(0).toFloat()
+    // var tensor3 = tensor.transpose().expandDims(0).toFloat()
+    
+    const warmup = tf.zeros([1,3,124,124])
+    // warmup.print()
+    // console.log("WARMUP")
+
+    // tensor.print()
+
+    // var val = await model.executeAsync(warmup)
+    // var val = model.predict(tensor2, 1, true);
+    // (val.sum()).print()
+    // val.print()
+
+
+    // const context = document.createElement('canvas').getContext('2d');
+    // context.drawImage(u, 0, 0);
+    // const {
+    //   data
+    // } = context.getImageData(10, 10, 1, 1);
+    // console.log(data)
     console.log("END")
   }
 
@@ -160,16 +182,17 @@ class Count extends Component{
     var val = this.state.value
     var image = this.state.image
     var display = this.state.display
-    console.log("DISPLAY :", display)
+    console.log(display)
+    console.log(image)
     return (
       display ?
       <div>
         <div>
           <h1>Live counting: select an image !</h1>
-          <input type="file" name="file" onChange={(e) => this.useModel(e)} />
+          <input type="file" name="file" onChange={this.useModel} />
         </div>
         <div> 
-          <img src={image} width="800" height="600"/>
+          <img src={image}/>
           <p> There are {val} persons on the picture </p>
         </div>
       </div>
